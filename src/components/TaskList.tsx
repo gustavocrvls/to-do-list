fc0b094706125f7e696c80ae6efe3cc3efe88f08
Gustavo Carvalho Silva <gustavocrvl42@gmail.com>
2021-03-12 14:13:53 -0300

    Funções de criar, checar e excluir notificações

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -15,15 +15,28 @@ export function TaskList() {
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (!newTaskTitle) return;
+
+    let task = {
+      id: new Date().getTime(), 
+      title: newTaskTitle, 
+      isComplete: false,
+    };
+
+    setTasks([...tasks, task]);
+    setNewTaskTitle('');
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    let tasksState = tasks;
+    tasksState = tasksState.map(task => task.id === id ? {...task, isComplete: !task.isComplete} : task);
+    setTasks(tasksState);
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    let tasksState = tasks;
+    tasksState = tasksState.filter(task => task.id !== id);
+    setTasks(tasksState);
   }
 
   return (
