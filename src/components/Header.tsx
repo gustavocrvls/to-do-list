fe607dc0f6648fd7ff0cfb239eac65af9399b1d1
Gustavo Carvalho Silva <gustavocrvl42@gmail.com>
2021-03-12 18:11:05 -0300

    create a server to dist

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,11 @@
 import '../styles/header.scss'
+const logo = require('../assets/logo.svg');
 
 export function Header() {
   return (
     <header className="header">
       <div>
-        <img src="/logo.svg" alt="to.do"/>
+        <img src={logo} alt="to.do"/>
       </div>
     </header>
   )
--- webpack.config.js
+++ webpack.config.js
@@ -44,6 +44,14 @@ module.exports = {
         exclude: /node_modules/,
         use: ['style-loader', 'css-loader', 'sass-loader']
       },
+      {
+        test: /\.svg$/,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
     ]
   }
 }
